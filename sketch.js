f693e379c2f72009edaba01a81a58bffffa19d87
randomqweasdzxc <vinhoecola@gmail.com>
2016-12-28 11:49:56 +0000

    sketch.js
    white rect

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function changeColor() {
 
 function draw() {
   background(bgcolor);
-  fill(255, 0, 0);
+  fill(255, 255, 255);
   rect(x, y, slider.value(), slider.value());
   h1.position(x, y);
   x += random(-3, 3);
