b24507051c2a26a91760ad5edb6af48ef1f90eac
randomqweasdzxc <vinhoecola@gmail.com>
2021-07-18 22:18:04 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,8 +24,7 @@ function changeColor() {
   bgcolor = color(random(0, 100));
   h1.html("Random Numbers");
   r = random(0, 100);
-  r.toFixed(0);
-  n1 = createP("Rolled: " + r);
+  n1 = createP("Rolled: " + r.toFixed(0));
   n1.style('font-size', '26px');
 }
 
